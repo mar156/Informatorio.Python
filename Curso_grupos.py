36b0cd10f0ee06ddd257689baad1f4af04ba9cfb
mar156 <m.n.v_156@hotmail.com.ar>
2020-07-13 12:29:42 -0300

    Modificaciones y elementos borrados para actualizar

--- Curso_grupos.py
+++ Curso_grupos.py
@@ -1,6 +1,6 @@
-nombre =str(input('Escriba por favor su nombre en letras minúsculas'))
+nombre =str(input('Escriba por favor su nombre en letras minúsculas: '))
 print()
-print('Si usted es del turno mañana, escriba 1')
+print('Si usted es del turno mañana, escriba 1: ')
 turno=str('Si usted es del turno tarde, escriba 2: ')
 words = nombre.split('') 
 characters = ['a''b''c''d''e''f''g''h''i''j''k''l''m''n''o''p''q''r''t''u''v''w''x''y''z']
